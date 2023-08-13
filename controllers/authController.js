3572f715413493a272dd28a5b0a903417ef39bac
Bartosz Jakubowski <bartoszj321@gmail.com>
2023-08-13 23:14:49 +0200

    Admin routers in progress

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,183 +1,147 @@
-require('dotenv').config();
+require("dotenv").config();
 const JWT_KEY = process.env.JWT_KEY;
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { response } = require("express");
 const { UserPassword, UserRegister, User } = require("../models/user");
+const { Calendar } = require("../models/calendar");
 
 const getAllData = async () => {
-  return Promise.all([
-    User.find(),
-    UserRegister.find(),
-    UserPassword.find(),
-  ])
-    .then(results => {
+  return Promise.all([User.find(), UserRegister.find(), UserPassword.find()])
+    .then((results) => {
       const user = results[0];
       const userRegister = results[1];
       const userPassword = results[2];
 
       return { user, userRegister, userPassword };
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       return false;
     });
 };
-const createToken = (user) =>
-{
-  const token = jwt.sign({user}, JWT_KEY, {
-    expiresIn: 900,
+const createToken = (user) => {
+  const token = jwt.sign({ user }, JWT_KEY, {
     expiresIn: 300000,
- })
+    // expiresIn: 300000,
+  });
 
- return token;
-}
+  return token;
+};
 
-module.exports.get_all = (req, res) => 
-{
+module.exports.get_all = (req, res) => {
   Promise.all([
     User.find(),
     UserRegister.find(),
-    UserPassword.find()
-  ])
-  .then(results => {
-    const user = results[0];
+    UserPassword.find(),
+    Calendar.find(),
+  ]).then((results) => {
+    const userConfirmed = results[0];
     const userRegister = results[1];
     const userPassword = results[2];
+    const calendars = results[3];
 
-    res.send({user, userRegister, userPassword});
+    res.send({ userConfirmed, userRegister, userPassword, calendars });
   });
 };
 
-module.exports.register_submit = (req, res) => 
-{
-    //to change
-    const email = req.body.mail;
-    User.findOne({ mail: email })
-      .then(user => {
-        if (!user) 
-        {
-          UserRegister.findOne({ mail: email })
-          .then(userRegister =>{
-            if (!userRegister)
-            {
+module.exports.register_submit = (req, res) => {
+  //to change
+  const email = req.body.mail;
+  User.findOne({ mail: email })
+    .then((user) => {
+      if (!user) {
+        UserRegister.findOne({ mail: email }).then((userRegister) => {
+          if (!userRegister) {
+            const userName = req.body.name;
+            const userMail = req.body.mail;
+            const userPassword = req.body.password;
+            const userPermissions = req.body.permissions;
+            const userRecords = req.body.records;
+            const registerUser = new UserRegister({
+              name: userName,
+              mail: userMail,
+              password: userPassword,
+              permissions: userPermissions,
+              records: userRecords,
+            });
 
-              const userName = req.body.name;
-              const userMail = req.body.mail;
-              const userPassword = req.body.password;
-              const userPermissions = req.body.permissions;
-              const userRecords = req.body.records;
-              const registerUser = new UserRegister({
-                name: userName, 
-                mail: userMail,
-                password: userPassword,
-                permissions: userPermissions,
-                records: userRecords
+            registerUser
+              .save()
+              .then((respond) => res.send({ message: "registerSuccess" }))
+              .catch((err) => {
+                console.log(err);
+                res.send({ message: "error" });
               });
-              
-              registerUser.save()
-              .then(respond => res.send({message: 'registerSuccess'}))
-              .catch(err => {console.log(err); res.send({message: 'error'})});
-            }
-            else
-            {
-              //user register already exist
-              res.send({message : 'registerErrorSend'});
-            }
-          })
-          
-        } 
-        else 
-        {
-          //user aLready exists
-          res.send({message : 'registerErrorUser'});
-        }
-      })
-      .catch(err => 
-        {
-          console.log(err);
-          res.send('error');
+          } else {
+            //user register already exist
+            res.send({ message: "registerErrorSend" });
+          }
         });
+      } else {
+        //user aLready exists
+        res.send({ message: "registerErrorUser" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("error");
+    });
 };
 
 module.exports.register_add = async (req, res) => {
-  const users = req.body;
-
-  for (let i = 0; i < users.length; i++) {
-    const user = users[i];
-    const userName = user.name;
-    const userMail = user.mail;
-    const userPassword = user.password;
-    const userPermissions = user.permissions;
-    const userRecords = user.records;
-    const userId = user._id;
-    const newUser = new User({
-      name: userName,
-      mail: userMail,
-      password: userPassword,
-      permissions: userPermissions,
-      records: userRecords,
-      _id: userId
+  const user = req.body;
+  const newUser = new User({
+    name: user.name,
+    mail: user.mail,
+    password: user.password,
+    permissions: ["user"],
+    records: [],
+  });
+  newUser
+    .save()
+    .then((newUser) => {
+      UserRegister.deleteOne({ _id: user._id })
+        .then((result) => res.send(newUser))
+        .catch((err) => {
+          console.log("err while removing user from register", err);
+          res.send(false);
+        });
+    })
+    .catch((err) => {
+      console.log("err while adding user from register", err);
+      res.send(false);
     });
+};
 
-    try {
-      await newUser.save();
-      await UserRegister.deleteOne({ _id: userId });
-    } catch (err) {
-      console.log(err);
-    }
-  }
+module.exports.register_delete = async (req, res) => {
+  const id = req.body.id;
+
+  await UserRegister.findByIdAndDelete(id).catch((err) => {
+    console.log(err);
+    res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
+  });
 
   try {
     const data = await getAllData();
-    if (users.length === 1) {
-      res.send({ data: data, message: 'Użytkownik został zarejestrowany' });
-    } else {
-      res.send({ data: data, message: 'Użytkownicy zostali zarejestrowani' });
-    }
-  } catch (err) {
-    console.log(err);
+    res.send({ data: data, message: "Prośba o reset hasła została usunięta" });
+  } catch (error) {
+    res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
   }
 };
 
-
-module.exports.register_delete = async  (req, res) =>
-{
-
-  const id = req.body.id;
-
-  await UserRegister.findByIdAndDelete(id)
-  .catch(err => 
-    {
-      console.log(err);
-      res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
-    })
-
-    try {
-      const data = await getAllData();
-      res.send({data: data, message: 'Prośba o reset hasła została usunięta'});
-    } catch (error) {
-      res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
-      
-    }
-};
-
-module.exports.password_submit = (req, res) => 
-{
-    const email = req.body.mail;
-    const password = req.body.password;
-    Promise.all([
-      User.findOne({ mail: email }),
-      UserPassword.findOne({ mail: email })
-    ])
-    .then(results => {
+module.exports.password_submit = (req, res) => {
+  const email = req.body.mail;
+  const password = req.body.password;
+  Promise.all([
+    User.findOne({ mail: email }),
+    UserPassword.findOne({ mail: email }),
+  ])
+    .then((results) => {
       const user = results[0];
       const userPassword = results[1];
 
-     if (user)
-     {
-        if (!userPassword)
-        {
-          
+      if (user) {
+        if (!userPassword) {
           const userName = user.name;
           const userMail = user.mail;
           const userPassword = password;
@@ -185,192 +149,173 @@ module.exports.password_submit = (req, res) =>
           const userRecords = user.records;
           const userId = user._id;
 
-          console.log(userName, userMail, userPassword, userPermissions, userRecords, userId);
-          const newUserPassword = new UserPassword(
-          {
+          console.log(
+            userName,
+            userMail,
+            userPassword,
+            userPermissions,
+            userRecords,
+            userId
+          );
+          const newUserPassword = new UserPassword({
             _id: userId,
             name: userName,
             mail: userMail,
-            password : userPassword,
+            password: userPassword,
             permissions: userPermissions,
             records: userRecords,
           });
-          newUserPassword.save()
-          .then(respond => res.send({message: 'passwordSuccess'}))
-          .catch(err => {console.log(err); res.send({message: 'error'})});
+          newUserPassword
+            .save()
+            .then((respond) => res.send({ message: "passwordSuccess" }))
+            .catch((err) => {
+              console.log(err);
+              res.send({ message: "error" });
+            });
+        } else {
+          res.send({ message: "passwordError" });
         }
-        else
-        {
-          res.send({message: 'passwordError'})
-        }
-     }
-     else
-     {
-      res.send({message: 'userNotFound'})
-     }
+      } else {
+        res.send({ message: "userNotFound" });
+      }
     })
-    .catch(err => 
-      {
-        console.log('database err', err)
-        res.send({message: 'error'});
-      });
+    .catch((err) => {
+      console.log("database err", err);
+      res.send({ message: "error" });
+    });
 };
 
-module.exports.password_add = async (req, res) => 
-{
+module.exports.password_add = async (req, res) => {
   const users = req.body;
-  try 
-  {
-    for (let i = 0; i < users.length; i++) 
-    {
-      
+  try {
+    for (let i = 0; i < users.length; i++) {
       const id = users[i]._id;
       const newPassword = users[i].password;
-      const updatedFields = {password: newPassword}
-        await User.findByIdAndUpdate(id, updatedFields, {new:true}).then(updatedUser => 
-          {
-            if (!updatedUser)
-              return false;
-            else
-            {
-              UserPassword.findByIdAndDelete(id).catch(err => console.log(err))
-            }
-              
-          })
-    }
-}
-catch (err) {
-  console.log(err);
-  return;
-}
-    getAllData()
-    .then(data=>
-      {
-        if (users.length === 1) {
-          res.send({ data: data, message: 'Nowe hasło zostało zapisane' });
-        } else {
-          res.send({ data: data, message: 'Nowe hasła zostały zapisane' });
+      const updatedFields = { password: newPassword };
+      await User.findByIdAndUpdate(id, updatedFields, { new: true }).then(
+        (updatedUser) => {
+          if (!updatedUser) return false;
+          else {
+            UserPassword.findByIdAndDelete(id).catch((err) => console.log(err));
+          }
         }
-      })    
-      .catch(err => console.log(err));
+      );
+    }
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+  getAllData()
+    .then((data) => {
+      if (users.length === 1) {
+        res.send({ data: data, message: "Nowe hasło zostało zapisane" });
+      } else {
+        res.send({ data: data, message: "Nowe hasła zostały zapisane" });
+      }
+    })
+    .catch((err) => console.log(err));
   //move from password to register
 };
 
-module.exports.password_delete = async (req, res) =>
-{
+module.exports.password_delete = async (req, res) => {
   const id = req.body.id;
 
-  await UserPassword.findByIdAndDelete(id)
-  .catch(err => 
-    {
-      console.log(err);
-      res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
-    })
+  await UserPassword.findByIdAndDelete(id).catch((err) => {
+    console.log(err);
+    res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
+  });
 
-
-    try {
-      const data = await getAllData();
-      res.send({data: data, message: 'Prośba o reset hasła została usunięta'});
-    } catch (error) {
-      res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
-      
-    }
+  try {
+    const data = await getAllData();
+    res.send({ data: data, message: "Prośba o reset hasła została usunięta" });
+  } catch (error) {
+    res.send("Wystąpił problem podczas usuwania prośby o reset hasła");
+  }
 };
 
-
 module.exports.user_add = async (req, res) => {
-    
-    const user = req.body;
-    const userName = user.name;
-    const userMail = user.mail;
-    const userPassword = user.password;
-    const userPermissions = user.permissions;
-    const userRecords = user.records;
-    const userId = user._id;
- 
-    const updatedFields = 
-      {
-        name: userName,
-        mail: userMail,
-        password: userPassword,
-        permissions: userPermissions,
-        records: userRecords,
-        _id: userId
+  const user = req.body;
+  const userName = user.name;
+  const userMail = user.mail;
+  const userPassword = user.password;
+  const userPermissions = user.permissions;
+  const userRecords = user.records;
+  const userId = user._id;
+
+  const updatedFields = {
+    name: userName,
+    mail: userMail,
+    password: userPassword,
+    permissions: userPermissions,
+    records: userRecords,
+    _id: userId,
+  };
+
+  try {
+    await User.findByIdAndUpdate(userId, updatedFields, { new: true }).then(
+      (updatedUser) => {
+        if (!updatedUser) {
+          console.log("tylko tu");
+          res.send(false);
+        }
       }
+    );
+  } catch (err) {
+    console.log(err);
+    res.send(false);
+    return;
+  }
 
-    try 
-    { 
-      await User.findByIdAndUpdate(userId, updatedFields, {new:true}).then(updatedUser => 
-        {
-          if (!updatedUser)
-          {
-            console.log('tylko tu')
-            res.send(false)
-          }
-        })
-    } catch (err) {
-      console.log(err);
-      res.send(false);
-      return;
-    }
-
-  try 
-  {
+  try {
     const data = await getAllData();
-    res.send({ data: data, message: 'Użytkownik został zaktualizowany' });
+    res.send({ data: data, message: "Użytkownik został zaktualizowany" });
   } catch (err) {
     console.log(err);
     res.send(false);
   }
 };
 
+module.exports.user_delete = async (req, res) => {
+  const userId = req.params.id;
 
-module.exports.user_delete = async  (req, res) =>
-{
-
-  const id = req.body.id;
-
-  await User.findByIdAndDelete(id)
-  .catch(err => 
-    {
-      console.log(err);
+  User.findByIdAndDelete(userId)
+    .then((result) => res.send(true))
+    .catch((err) => {
+      console.log("user delete error", err);
       res.send(false);
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { mail, password } = req.body;
+  User.findOne({ mail })
+    .then((user) => {
+      if (user.password === password) {
+        const token = createToken(user);
+        res.send({ token, message: "loginSuccess" });
+      } else res.send({ token: false, message: "validData" });
     })
+    .catch((err) => {
+      console.log(err);
+      res.send({ auth: false, token: false, message: "userNotFound" });
+    });
+};
 
-    try {
-      const data = await getAllData();
-      res.send({data: data, message: 'Użytkownik został usunięty'});
-    } catch (error) {
+module.exports.user_update = (req, res) => {
+  console.log("halo");
+  const user = req.body;
+  const userId = req.params.id;
+
+  User.updateOne({ _id: userId }, user)
+    .then((result) => res.send(result))
+    .catch((err) => {
+      console.log("add user erro");
       res.send(false);
-      
-    }
+    });
 };
 
-module.exports.login = (req, res) => 
-{
-      const {mail, password} = req.body;
-      User.findOne({ mail })
-      .then(user => 
-        {
-          if (user.password === password)
-          {
-            const token = createToken(user);
-            res.send({token, message: 'loginSuccess'});
-          }
-          else
-            res.send({token: false, message: 'validData'})
-
-        })
-      .catch(err=>
-        {
-          console.log(err);
-          res.send({auth: false, token: false, message: 'userNotFound'})
-        });
-};
-
-module.exports.token = (req, res) =>
-{
+module.exports.token = (req, res) => {
   const user = req.body;
   const token = createToken(user);
-  res.send({token});
-}
+  res.send({ token });
+};
--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 const JWT_KEY = process.env.JWT_KEY;
 const DB_LOGIN = process.env.DB_LOGIN;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
-const express = require ('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const calendarRoutes = require('./routes/calendarRoutes');
-const authRoutes = require('./routes/authRoutes');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const path = require('path');
+const mongoose = require("mongoose");
+const calendarRoutes = require("./routes/calendarRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const path = require("path");
 //for websockets
 const server = require("http").Server(app);
 const websocket = require("./websockets/websockets");
 const io = require("socket.io")(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 app.use(express.json());
 app.use(cors());
- 
 
 // app.use((req, res, next) => {
 //   if (req.method === 'GET') {
@@ -32,73 +31,75 @@ app.use(cors());
 //   // next();
 // });
 
-app.use(express.static(path.join(__dirname, 'build')));
-const routesToPassWithoutJWT = ['/login', '/logowanie', '/password/submit', '/register/submit']
-const knownRoutes = ['/login', '/logowanie', '/password/submit', '/register/submit']
-const verifyJWT = (req, res, next) =>
-{ 
-
-  if(req.method === 'GET')
-  {
+// app.use(express.static(path.join(__dirname, 'build')));
+const routesToPassWithoutJWT = [
+  "/login",
+  "/logowanie",
+  "/password/submit",
+  "/register/submit",
+];
+const knownRoutes = [
+  "/login",
+  "/logowanie",
+  "/password/submit",
+  "/register/submit",
+];
+const verifyJWT = (req, res, next) => {
+  if (req.method === "GET") {
     next();
     return;
   }
 
-
-  if(routesToPassWithoutJWT.includes(req.originalUrl))
-  {
+  if (routesToPassWithoutJWT.includes(req.originalUrl)) {
     next();
     return;
   }
 
-  const token = req.headers['x-access-token'];
-  
-  if (!token)
-  {
-    res.status(401).json({ auth: false, message: 'Failed to authenticate' });
+  const token = req.headers["x-access-token"];
+
+  if (!token) {
+    res.status(401).json({ auth: false, message: "Failed to authenticate" });
     return;
   }
-  
-  jwt.verify(token, JWT_KEY, (err, decoded) =>
-  {
-    if (err)
-    {
-      res.status(401).json({ auth: false, message: 'Failed to authenticate' });
+
+  jwt.verify(token, JWT_KEY, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ auth: false, message: "Failed to authenticate" });
       return;
     }
     // const decodedToken = jwt.decode(token);
-    // const currentTime = Math.floor(Date.now() / 1000); 
-    // const expiresIn = decodedToken.exp - currentTime; 
+    // const currentTime = Math.floor(Date.now() / 1000);
+    // const expiresIn = decodedToken.exp - currentTime;
 
     // req.id = decoded.user.id;
     next();
-  })
-
-}
+  });
+};
 // import jwt from 'jsonwebtoken';
 // const authRoutes = require('./routes/authRoutes.js');
 
 app.use(verifyJWT);
 
-// app.use(express.static('public'));
+app.use(express.static("public"));
 
 const dbURI = `mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@calendar.va1iidg.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((resault) => 
-{
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((resault) => {
     // app.listen(3003, ()=> console.log('serwer działa na porcie 3003'));
-})
-.catch(err=> console.log(err))
+  })
+  .catch((err) => console.log(err));
 
 app.use(authRoutes);
-app.use('/calendar', calendarRoutes);
+app.use("/calendar", calendarRoutes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 websocket(io);
 // app.use(authRoutes);
 // server.listen(process.env.PORT || 3002, () => console.log('server działa, port 3002'));
-server.listen(process.env.PORT || 3000, () => console.log('server działa, domyślnie port 3000'));
-
+server.listen(process.env.PORT || 3000, () =>
+  console.log("server działa, domyślnie port 3000")
+);
